803b936591cae557d4a6ab1ea4fd90272a3180c3
Yuri Brito <yuriabrito@gmail.com>
2021-04-06 19:11:12 -0300

    solving three cases with new API

--- match_test.go
+++ match_test.go
@@ -2,6 +2,7 @@ package match_test
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 
 	ma "github.com/ghostec/match"
@@ -28,7 +29,7 @@ func TestFibonacci(t *testing.T) {
 		t.Run(fmt.Sprintf("%d", tc.n), func(t *testing.T) {
 			fn := Fibonacci(tc.n)
 			if fn != tc.fn {
-				t.Errorf("unexpected fib(%d). Got: %d, Expected: %d", tc.n, fn, tc.fn)
+				t.Errorf("unexpected output. Got: %d, Expected: %d", fn, tc.fn)
 			}
 		})
 	}
@@ -37,38 +38,101 @@ func TestFibonacci(t *testing.T) {
 var fibonacci = ma.Match{
 	{ma.When(0), 0},
 	{ma.When(1), 1},
-	{ma.When(ma.Any(0)), func(m *ma.Match, n int) int { return m.Int(n-1) + m.Int(n-2) }},
+	{ma.When(ma.Any(0)), func(m ma.Match, n int) int {
+		return m.Int(n-1) + m.Int(n-2)
+	}},
 }
 
 func Fibonacci(n int) (fn int) {
 	return fibonacci.Int(n)
 }
 
+func TestJoin(t *testing.T) {
+	testCases := []struct {
+		input  interface{}
+		by     string
+		output string
+	}{
+		{
+			input:  []int{0, 1, 2, 3, 4, 5},
+			by:     "",
+			output: "012345",
+		},
+		{
+			input:  []string{"Mr", "John", "Doe"},
+			by:     " ",
+			output: "Mr John Doe",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.output, func(t *testing.T) {
+			output := Join(tc.input, tc.by)
+			if output != tc.output {
+				t.Errorf("unexpected output. Got: %s, Expected: %s", output, tc.output)
+			}
+		})
+	}
+}
+
 var join = ma.Match{
 	{
-		ma.When(ma.Empty, ma.String, ma.Slice(0)),
+		ma.When(ma.Empty, ma.String, ma.String(0)),
 		func(joined string) string {
 			return joined
 		},
 	},
 	{
 		ma.When(ma.Slice(0), ma.String(1)),
-		func(m *ma.Match, list interface{}, by string) string {
+		func(m ma.Match, list interface{}, by string) string {
 			return m.String(list, by, "")
 		},
 	},
 	{
-		ma.When(ma.Slice(ma.Head(0, 1), ma.Slice(1)), ma.String(2), ma.Empty),
-		func(m *ma.Match, head, tail interface{}, by string) string {
+		ma.When(
+			ma.Slice(ma.Head(0, 1), ma.Slice(1)),
+			ma.String(2),
+			ma.Empty,
+		),
+		func(m ma.Match, head, tail interface{}, by string) string {
 			return m.String(tail, by, fmt.Sprintf("%v", head))
 		},
 	},
 	{
-		ma.When(ma.Slice(ma.Head(0, 1), ma.Slice(1)), ma.String(2), ma.String(3)),
-		func(m *ma.Match, head, tail interface{}, by, acc string) string {
+		ma.When(
+			ma.Slice(ma.Head(0, 1), ma.Slice(1)),
+			ma.String(2),
+			ma.String(3),
+		),
+		func(m ma.Match, head, tail interface{}, by, acc string) string {
 			return m.String(tail, by, fmt.Sprintf("%s%s%v", acc, by, head))
 		},
 	},
+}
+
+func Join(slice interface{}, by string) string {
+	return join.String(slice, by)
+}
+
+func TestReverse(t *testing.T) {
+	testCases := []struct {
+		input  interface{}
+		output interface{}
+	}{
+		{
+			input:  []int{0, 1, 2, 3, 4, 5},
+			output: []int{5, 4, 3, 2, 1, 0},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(Join(tc.input, ", "), func(t *testing.T) {
+			output := Reverse(tc.input)
+			if reflect.DeepEqual(output, tc.output) {
+				t.Errorf("unexpected output. Got: %s, Expected: %s", output, tc.output)
+			}
+		})
+	}
 }
 
 var reverse = ma.Match{
@@ -82,7 +146,7 @@ var reverse = ma.Match{
 		ma.When(
 			ma.Slice(ma.Slice(0), ma.Tail(1, 1)),
 		),
-		func(m *ma.Match, head *ma.SliceType, tail interface{}) *ma.SliceType {
+		func(m ma.Match, head *ma.SliceType, tail interface{}) *ma.SliceType {
 			return m.Slice(head, tail)
 		},
 	},
@@ -91,8 +155,12 @@ var reverse = ma.Match{
 			ma.Slice(ma.Slice(0), ma.Tail(1, 1)),
 			ma.Slice(2),
 		),
-		func(m *ma.Match, head *ma.SliceType, tail interface{}, acc *ma.SliceType) *ma.SliceType {
+		func(m ma.Match, head *ma.SliceType, tail interface{}, acc *ma.SliceType) *ma.SliceType {
 			return m.Slice(head, acc.Append(tail))
 		},
 	},
+}
+
+func Reverse(list interface{}) interface{} {
+	return reverse.Slice(list).Get()
 }
--- when.go
+++ when.go
@@ -1,5 +1,9 @@
 package match
 
+import (
+	"reflect"
+)
+
 func When(args ...interface{}) when {
 	all := make([]interface{}, len(args))
 	for i := range args {
@@ -10,12 +14,49 @@ func When(args ...interface{}) when {
 
 type when []interface{}
 
-func (w when) match(args []interface) bool {
+func (w when) match(args []interface{}) (ok bool, wargs []interface{}) {
 	if len(args) != len(w) {
-		return false
+		return false, nil
 	}
 
-	for i := range args {
+	var matchedArgs []Arg
 
+	for i := range w {
+		h, isHandle := w[i].(handle)
+
+		wiValue := reflect.ValueOf(w[i])
+
+		isAnyFunc := wiValue.Kind() == reflect.Func && reflect.ValueOf(Any).Pointer() == wiValue.Pointer()
+		isEmptyFunc := wiValue.Kind() == reflect.Func && reflect.ValueOf(Empty).Pointer() == wiValue.Pointer()
+		isSliceFunc := wiValue.Kind() == reflect.Func && reflect.ValueOf(Slice).Pointer() == wiValue.Pointer()
+		isStringFunc := wiValue.Kind() == reflect.Func && reflect.ValueOf(String).Pointer() == wiValue.Pointer()
+
+		switch {
+		case isEmptyFunc:
+			h = handle{kind: hkEmpty}
+		case isSliceFunc:
+			h = handle{kind: hkSlice}
+		case isStringFunc:
+			h = handle{kind: hkString}
+		case isAnyFunc, reflect.DeepEqual(w[i], args[i]):
+			continue
+		case !isHandle:
+			return false, nil
+		}
+
+		hargs, err := h.Match(args[i])
+		if err != nil {
+			return false, nil
+		}
+
+		matchedArgs = append(matchedArgs, hargs...)
 	}
+
+	wargs = make([]interface{}, len(matchedArgs))
+
+	for _, arg := range matchedArgs {
+		wargs[arg.N] = arg.Value
+	}
+
+	return true, wargs
 }
