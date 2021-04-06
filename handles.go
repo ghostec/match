e25f070eb318e7e1a31de44d30a000052c249c03
Yuri Brito <yuriabrito@gmail.com>
2021-04-06 19:11:12 -0300

    solving three cases with new API

--- handles.go
+++ handles.go
@@ -1,10 +1,14 @@
 package match
 
-import "reflect"
+import (
+	"errors"
+	"reflect"
+)
 
 type handle struct {
 	kind     handleKind
 	argn     *int
+	size     int
 	children []interface{}
 }
 
@@ -57,14 +61,46 @@ func String(args ...interface{}) (ret handle) {
 }
 
 func Slice(args ...interface{}) (ret handle) {
-	switch len(args) {
-	case 0:
-		ret.kind = hkSlice
+	ret.kind = hkSlice
+
+	if len(args) == 0 {
 		return
+	}
+
+	switch len(args) {
 	case 1:
-		if argn, ok := args[0].(int); ok {
+		argn, ok := args[0].(int)
+		if !ok {
+			panic("only argument must be argn")
+		}
+
+		ret.argn = &argn
+	default:
+		argn, ok := args[0].(int)
+		if ok {
 			ret.argn = &argn
-			ret.kind = hkSlice
+		}
+
+		for i := range args {
+			argValue := reflect.ValueOf(args[i])
+			isArgFunc := argValue.Kind() == reflect.Func
+
+			isHeadFunc := isArgFunc && argValue.Pointer() == reflect.ValueOf(Head).Pointer()
+			isTailFunc := isArgFunc && argValue.Pointer() == reflect.ValueOf(Tail).Pointer()
+			isSliceFunc := isArgFunc && argValue.Pointer() == reflect.ValueOf(Slice).Pointer()
+
+			h, isHandle := args[i].(handle)
+
+			isHead := isHeadFunc || (isHandle && h.kind == hkHead)
+			isTail := isTailFunc || (isHandle && h.kind == hkTail)
+			isSlice := isSliceFunc || (isHandle && h.kind == hkSlice)
+
+			switch {
+			case isHead, isTail, isSlice:
+				ret.children = append(ret.children, args[i])
+			default:
+				panic("not head, tail or slice")
+			}
 		}
 	}
 
@@ -73,8 +109,9 @@ func Slice(args ...interface{}) (ret handle) {
 
 func Head(argn, size int) (ret handle) {
 	ret.kind = hkHead
+	ret.size = size
 
-	if argn > 0 {
+	if argn >= 0 {
 		ret.argn = &argn
 	}
 
@@ -83,6 +120,7 @@ func Head(argn, size int) (ret handle) {
 
 func Tail(argn, size int) (ret handle) {
 	ret.kind = hkTail
+	ret.size = size
 
 	if argn > 0 {
 		ret.argn = &argn
@@ -93,6 +131,10 @@ func Tail(argn, size int) (ret handle) {
 
 type SliceType struct {
 	wrapped interface{}
+}
+
+func (sl *SliceType) Get() interface{} {
+	return sl.wrapped
 }
 
 func NewSliceType(wrapped interface{}) *SliceType {
@@ -107,4 +149,155 @@ func (sl *SliceType) Append(el interface{}) *SliceType {
 	ret := reflect.Append(reflect.ValueOf(sl.wrapped), reflect.ValueOf(el))
 	sl.wrapped = ret.Interface()
 	return sl
+}
+
+func (h handle) Match(arg interface{}) (ret []Arg, err error) {
+	switch h.kind {
+	case hkEmpty:
+		argValue := reflect.ValueOf(arg)
+		switch argValue.Kind() {
+		case reflect.Array, reflect.Chan, reflect.Map, reflect.Slice, reflect.String:
+			if argValue.Len() == 0 {
+				return nil, nil
+			}
+		}
+
+		return nil, errors.New("not empty")
+
+	case hkAny:
+		if h.argn != nil {
+			ret = append(ret, Arg{N: *h.argn, Value: arg})
+		}
+
+	case hkString:
+		if _, ok := arg.(string); !ok {
+			return nil, errors.New("arg isn't string")
+		}
+
+		if h.argn != nil {
+			ret = append(ret, Arg{N: *h.argn, Value: arg})
+		}
+
+	case hkHead, hkTail:
+		argValue := reflect.ValueOf(arg)
+
+		if argValue.Kind() != reflect.Slice {
+			return nil, errors.New("arg isn't slice")
+		}
+
+		var value interface{}
+
+		if h.size == 1 {
+			switch h.kind {
+			case hkHead:
+				value = argValue.Index(0)
+			case hkTail:
+				value = argValue.Index(argValue.Len() - 1)
+			}
+		} else {
+			switch h.kind {
+			case hkHead:
+				ret = append(ret, Arg{N: *h.argn, Value: argValue.Slice(0, h.size).Interface()})
+			case hkTail:
+				l := argValue.Len()
+				ret = append(ret, Arg{N: *h.argn, Value: argValue.Slice(l-h.size, l).Interface()})
+			}
+		}
+
+		ret = append(ret, Arg{N: *h.argn, Value: value})
+
+	case hkSlice:
+		argValue := reflect.ValueOf(arg)
+
+		if argValue.Kind() != reflect.Slice {
+			return nil, errors.New("arg isn't slice")
+		}
+
+		if h.argn == nil && len(h.children) == 0 {
+			return
+		}
+
+		if len(h.children) == 1 {
+			return nil, errors.New("slice can't be single child of slice")
+		}
+
+		if h.argn != nil {
+			ret = append(ret, Arg{
+				N:     *h.argn,
+				Value: arg,
+			})
+		}
+
+		for i := range h.children {
+			hh, ok := h.children[i].(handle)
+			if !ok {
+				continue
+			}
+
+			switch hh.kind {
+			case hkSlice:
+				start, end := -1, -1
+
+				if i == 0 {
+					next := h.children[1]
+					nextValue := reflect.ValueOf(next)
+					isNextFunc := nextValue.Kind() == reflect.Func
+
+					isTailFunc := isNextFunc && nextValue.Pointer() == reflect.ValueOf(Tail).Pointer()
+					hh, isHandle := next.(handle)
+					isTail := isTailFunc || (isHandle && hh.kind == hkTail)
+
+					if !isTail {
+						return nil, errors.New("slice neighbors must be head or tail")
+					}
+
+					size := 1
+					if !isTailFunc {
+						size = hh.size
+					}
+
+					start, end = 0, argValue.Len()-size
+				} else {
+					prev := h.children[0]
+					prevValue := reflect.ValueOf(prev)
+					isPrevFunc := prevValue.Kind() == reflect.Func
+
+					isHeadFunc := isPrevFunc && prevValue.Pointer() == reflect.ValueOf(Head).Pointer()
+					hh, isHandle := prev.(handle)
+					isHead := isHeadFunc || (isHandle && hh.kind == hkHead)
+
+					if !isHead {
+						return nil, errors.New("slice neighbors must be head or tail")
+					}
+
+					size := 1
+					if !isHeadFunc {
+						size = hh.size
+					}
+
+					start, end = size, argValue.Len()
+				}
+
+				ret = append(ret, Arg{
+					N:     *hh.argn,
+					Value: argValue.Slice(start, end).Interface(),
+				})
+
+			case hkHead, hkTail:
+				hargs, err := hh.Match(arg)
+				if err != nil {
+					return nil, err
+				}
+
+				ret = append(ret, hargs...)
+			}
+		}
+	}
+
+	return
+}
+
+type Arg struct {
+	N     int
+	Value interface{}
 }
--- match.go
+++ match.go
@@ -1,5 +1,10 @@
 package match
 
+import (
+	"errors"
+	"reflect"
+)
+
 type Match []Case
 
 type Case struct {
@@ -8,13 +13,88 @@ type Case struct {
 }
 
 func (m Match) Int(args ...interface{}) int {
-	return 0
+	res, err := m.Result(args...)
+	if err != nil {
+		panic(err)
+	}
+	val, ok := res.(int)
+	if !ok {
+		panic("not int")
+	}
+	return val
 }
 
 func (m Match) String(args ...interface{}) string {
-	return ""
+	res, err := m.Result(args...)
+	if err != nil {
+		panic(err)
+	}
+	val, ok := res.(string)
+	if !ok {
+		panic("not string")
+	}
+	return val
 }
 
 func (m Match) Slice(args ...interface{}) *SliceType {
 	return NewSliceType([]interface{}{})
+}
+
+func (m Match) Result(args ...interface{}) (interface{}, error) {
+	c := m.match(args)
+
+	if c == nil {
+		return nil, errors.New("not matched")
+	}
+
+	dotype := reflect.TypeOf(c.Do)
+	if dotype.Kind() != reflect.Func {
+		return c.Do, nil
+	}
+
+	// from now on, c.Do is a func
+
+	dovalue := reflect.ValueOf(c.Do)
+
+	input := make([]reflect.Value, dotype.NumIn())
+
+	rshift := 0
+	if dotype.NumIn() > 0 && dotype.In(0) == reflect.TypeOf(Match{}) {
+		input[rshift] = reflect.ValueOf(m)
+		rshift += 1
+	}
+
+	for i := range c.Args {
+		input[rshift+i] = reflect.ValueOf(c.Args[i])
+	}
+
+	out := dovalue.Call(input)
+
+	switch len(out) {
+	case 1:
+		return out[0].Interface(), nil
+	case 2:
+		return out[0].Interface(), out[1].Interface().(error)
+	default:
+		return nil, errors.New("more than 2 outputs")
+	}
+}
+
+func (m Match) match(args []interface{}) *CaseWithArgs {
+	for i := range m {
+		ok, cargs := m[i].When.match(args)
+		if ok {
+			return &CaseWithArgs{
+				Case: &m[i],
+				Args: cargs,
+			}
+		}
+	}
+
+	return nil
+}
+
+type CaseWithArgs struct {
+	*Case
+	Args []interface{}
 }
